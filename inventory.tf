2b0d4e3d9c995a35a59f13c2a7245bf330f0b8d5
Mufeednpd <90769002+Mufeednpd@users.noreply.github.com>
2023-10-23 08:19:00 +0530

    Create inventory.tf

---
+++ inventory.tf
@@ -0,0 +1,12 @@
+# Generate ansible inventory file
+
+resource "local_file" "ansible_inventory" {
+  content = templatefile(
+    "${path.cwd}/ansible/hosts.tftpl", {
+      hostname    = "worker", workernode_ip = "${data.aws_network_interface.k8worker.*.private_ip}",
+      k8master_ip = "${data.aws_network_interface.k8master.*.private_ip}",
+      jenkins_ip  = "${data.aws_network_interface.jenkins.*.private_ip}"
+  })
+  filename   = "${path.cwd}/ansible/hosts"
+  depends_on = [ aws_instance.jenkins]
+}
