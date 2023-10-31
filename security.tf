3a5981ad255ced296c3a376db45fe23b6bf0f25a
Mufeednpd <90769002+Mufeednpd@users.noreply.github.com>
2023-10-31 12:24:14 +0530

    Update security.tf

--- security.tf
+++ security.tf
@@ -15,5 +15,12 @@ resource "aws_security_group" "management" {
   tags = {
     Name = "Allow inbound  management traffic"
   }
+
+egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] # Allow outbound traffic to the internet
+  }
 }
 
