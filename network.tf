6111033bc6fd4b27650b0e10d6179908657aaff6
Mufeednpd <90769002+Mufeednpd@users.noreply.github.com>
2023-11-02 07:37:52 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -68,6 +68,10 @@ resource "aws_route_table" "public" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
+  route {
+    cidr_block                = "172.16.0.0/25"
+    vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+  }
 
 
 }
