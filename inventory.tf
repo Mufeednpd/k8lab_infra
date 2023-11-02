b5f101948b1fcfd73f5eda952c902796a0fcfc8c
Mufeednpd <90769002+Mufeednpd@users.noreply.github.com>
2023-11-02 07:26:47 +0530

    Update inventory.tf

--- inventory.tf
+++ inventory.tf
@@ -3,9 +3,9 @@
 resource "local_file" "ansible_inventory" {
   content = templatefile(
     "${path.cwd}/ansible/hosts.tftpl", {
-      hostname    = "worker", workernode_ip = "${data.aws_instance.k8worker.*.public_ip}",
-      k8master_ip = "${data.aws_instance.k8master.*.public_ip}",
-      jenkins_ip  = "${data.aws_instance.jenkins.*.public_ip}"
+      hostname    = "worker", workernode_ip = "${data.aws_instance.k8worker.*.private_ip}",
+      k8master_ip = "${data.aws_instance.k8master.*.private_ip}",
+      jenkins_ip  = "${data.aws_instance.jenkins.*.private_ip}"
   })
   filename   = "${path.cwd}/ansible/hosts"
   depends_on = [ aws_instance.jenkins]
