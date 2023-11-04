2f988e52441fb46e4b5202e3f308e429770003c9
Mufeednpd <90769002+Mufeednpd@users.noreply.github.com>
2023-11-04 20:33:44 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -22,7 +22,7 @@ resource "aws_lb" "application_alb" {
     internal   = false
     ip_address_type = "ipv4"
     load_balancer_type  = "application"
-    subnets      = ["pub_subnet", pub_subnet2"]
+    subnets      = ["pub_subnet", "pub_subnet2"]
 }
 
 resource "aws_lb_listener" "application_alb_listener" {
