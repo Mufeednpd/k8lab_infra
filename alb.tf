2a4c090dc81bba72ff7d86ac592b2ce800d7416a
Mufeednpd <90769002+Mufeednpd@users.noreply.github.com>
2023-11-06 19:12:44 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -1,54 +1,63 @@
-#target group
-resource "aws_lb_target_group" "worker_target_group" {
-    health_check {
-       interval  = 10
-       path      = "/"
-       protocol  = "HTTP"
-       timeout   = 5
-       healthy_threshold = 5
-       unhealthy_threshold = 2
-    }
 
-     name  = "worker-target-group"
-     port  = 30259
-     protocol = "HTTP"
-     target_type  = "instance"
-     vpc_id  = aws_vpc.vpc.id
-}
+# # create alb target group for k8workers
 
-#Creating ALB
-resource "aws_lb" "application_alb" {
-    name  = "app-alb"
-    internal   = false
-    ip_address_type = "ipv4"
-    load_balancer_type  = "application"
-    subnets      = ["subnet-0193368b0ed42ac2c", "subnet-019a195c1f5e07fc0"]
-}
+# resource "aws_lb_target_group" "k8workers" {
+#   name     = "k8workers"
+#   port     = 30259
+#   protocol = "HTTP"
+#   vpc_id   = aws_vpc.vpc.id
+#   target_type = "instance"
+  
+# }
 
-resource "aws_lb_listener" "application_alb_listener" {
-  load_balancer_arn = aws_lb.application_alb.arn
-  port              = 80
-  protocol          = "HTTP"
-  default_action {
-    type             = "fixed-response"
-    fixed_response {
-      content_type = "text/plain"
-      status_code  = "200"
-      message_body = "OK"
-    }
-  }
-}
-    
-resource "aws_lb_listener_rule" "app_listener_rule" {
-  listener_arn = aws_lb_listener.application_alb_listener.arn
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.worker_target_group.arn
-  }
 
-  condition {
-    path_pattern {
-      values = ["/"]
-    }
-  }
-}
+# resource "aws_lb" "alb" {
+#   name               = "weblb"
+#   internal           = false
+#   load_balancer_type = "application"
+#   security_groups    = [aws_security_group.alb.id]
+#   subnets            = [aws_subnet.pub_subnet1.id,aws_subnet.pub_subnet2.id]
+
+#   tags = {
+#     Environment = "dev"
+#   }
+# }
+
+
+# # attach  k8 worker nodes in AZ a
+
+
+
+# resource "aws_lb_target_group_attachment" "k8workers_az_a" {
+#   count = 1
+#   target_group_arn = aws_lb_target_group.k8workers.arn
+#   target_id        = aws_instance.k8workers_az_a[count.index].id
+#   port             = 30259
+# }
+
+
+# # attach  k8 worker nodes in AZ b
+
+
+
+# resource "aws_lb_target_group_attachment" "k8workers_az_b" {
+#   count = 1
+#   target_group_arn = aws_lb_target_group.k8workers.arn
+#   target_id        = aws_instance.k8workers_az_b[count.index].id
+#   port             = 30259
+# }
+
+
+# # creat listener for k8 workers
+
+# resource "aws_lb_listener" "front_end" {
+#   load_balancer_arn = aws_lb.alb.arn
+#   port              = "443"
+#   protocol          = "HTTPS"
+#   certificate_arn   = "arn:aws:acm:us-east-1:278004647125:certificate/dc2f58c4-bbd0-4eab-8a46-cc60901bb169"
+
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.k8workers.arn
+#   }
+# }
