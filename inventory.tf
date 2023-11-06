c46fb33665fd59e69cdc16bf60380b2b1b5d62cd
Valappil <mvalappil@clinipace.com>
2023-11-06 21:17:45 +0530

    k8worker

--- inventory.tf
+++ inventory.tf
@@ -4,8 +4,8 @@ resource "local_file" "ansible_inventory" {
   content = templatefile(
     "${path.cwd}/ansible/hosts.tftpl", {
       hostname    = "worker", 
-      workernode_az_a_ip = "${data.aws_instance.k8workers_az_a.*.private_ip}",
-      workernode_az_b_ip = "${data.aws_instance.k8workers_az_b.*.private_ip}"
+      k8worker_az_a_ip = "${data.aws_instance.k8workers_az_a.*.private_ip}",
+      k8worker_az_b_ip = "${data.aws_instance.k8workers_az_b.*.private_ip}"
       k8master_ip = "${data.aws_instance.k8master.*.private_ip}",
       jenkins_ip  = "${data.aws_instance.jenkins.*.private_ip}"
   })
