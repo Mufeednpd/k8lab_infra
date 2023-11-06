9e9c98b1732d0c90f0bdf158e68d87faba383ad3
Valappil <mvalappil@clinipace.com>
2023-11-06 21:10:44 +0530

    sd

--- compute.tf
+++ compute.tf
@@ -47,20 +47,27 @@ resource "aws_instance" "k8master" {
 
 # ec2 instace for k8 worker nodes
 
-resource "aws_instance" "k8worker" {
-  count         = 2
+
+resource "aws_instance" "k8workers_az_a" {
+  count         = 1
   ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
   key_name      = "newkey"
+  iam_instance_profile = "k8_instance_profile"
   associate_public_ip_address = "true"
   vpc_security_group_ids = [aws_security_group.management.id,aws_security_group.k8cluster.id]
  subnet_id = aws_subnet.pub_subnet.id
 
   root_block_device {
     volume_type           = "gp2"
-    volume_size           = 40
+    volume_size           = 15
     delete_on_termination = true
   }
+
+  user_data = <<-EOF
+  #!/bin/bash
+  hostnamectl set-hostname "k8worker${count.index+1}"
+  EOF
 
   tags = {
     Name = "${var.Env}_k8worker${count.index + 1}"
@@ -69,10 +76,45 @@ resource "aws_instance" "k8worker" {
   }
 }
 
-# query EC2 for k8worker nodes to get private ip
-data "aws_instance" "k8worker" {
-  count = 2
-  instance_id    = aws_instance.k8worker[count.index].id
+resource "aws_instance" "k8workers_az_b" {
+  count         = 1
+  ami           = "ami-053b0d53c279acc90"
+  instance_type = "t2.micro"
+  key_name      = "newkey"
+  iam_instance_profile = "k8_instance_profile"
+  associate_public_ip_address = "true"
+  vpc_security_group_ids = [aws_security_group.management.id,aws_security_group.k8cluster.id]
+ subnet_id = aws_subnet.pub_subnet.id
+
+  root_block_device {
+    volume_type           = "gp2"
+    volume_size           = 15
+    delete_on_termination = true
+  }
+
+  user_data = <<-EOF
+  #!/bin/bash
+  hostnamectl set-hostname "k8worker${count.index+2}"
+  EOF
+
+  tags = {
+    Name = "${var.Env}_k8worker${count.index + 2}"
+    Env  = var.Env
+    Role = "k8worker"
+  }
+}
+
+
+# query EC2 for k8worker nodes in AZ a to get private ip
+data "aws_instance" "k8workers_az_a" {
+  count = 1
+  instance_id    = aws_instance.k8workers_az_a[count.index].id
+}
+
+# query EC2 for k8worker nodes in AZ b to get private ip
+data "aws_instance" "k8workers_az_b" {
+  count = 1
+  instance_id    = aws_instance.k8workers_az_b[count.index].id
 }
 
 # query EC2  for k8master to get private ip
