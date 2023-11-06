f4ee066e2b04e72329915d5ff8e6f8b84c403910
Mufeednpd <90769002+Mufeednpd@users.noreply.github.com>
2023-11-06 19:20:29 +0530

    Update security.tf

--- security.tf
+++ security.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "management" {
   name        = "management_sg"
-  description = "Allow inbound management traffic"
+  description = "allow inbound ssh"
   vpc_id      = aws_vpc.vpc.id
 
   ingress {
@@ -8,48 +8,77 @@ resource "aws_security_group" "management" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["172.16.0.0/24"]
   }
 
 
   tags = {
-    Name = "Allow inbound  management traffic"
-  }
-
-egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] # Allow outbound traffic to the internet
+    Name = "allow inbound ssh"
   }
 }
 
+# security group to allow k8cluster inbound
 
 resource "aws_security_group" "k8cluster" {
-  name        = "k8cluster_sg"
-  description = "Allow inbound k8 traffic"
+  name        = "k8cluster_inbound"
+  description = "allow ingress to k8cluster"
   vpc_id      = aws_vpc.vpc.id
 
   ingress {
-    description = "Allow 10250"
-    from_port   = 0
-    to_port     = 65534
+    description = "nginx from alb"
+    from_port   = 31975
+    to_port     = 31975
     protocol    = "tcp"
-    self = "true"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "allow ssh from jenkins_master"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["172.16.0.0/24"]
   }
 
 
   tags = {
-    Name = "Allow inbound  k8 traffic"
+    Name = "Allow inbound  web traffic"
   }
 
 }
 
-resource "aws_security_group" "alb_sg" {
-  name        = "alb-security-group"
-  description = "Security group for the Application Load Balancer"
+# security group to allow appolcation load balancer inbound
+
+resource "aws_security_group" "alb" {
+  name        = "http_inbound_sg"
+  description = "Allow inbound http traffic"
   vpc_id      = aws_vpc.vpc.id
 
-  // Define inbound and outbound rules for your security group if needed
-}
+  ingress {
+    description = "allow http from internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
 
+  }
+
+  ingress {
+    description = "allow http from internet"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+  egress {
+    from_port        = 31975
+    to_port          = 31975
+    protocol         = "tcp"
+    security_groups      = [aws_security_group.k8cluster.id]
+  }
+
+  tags = {
+    Name = "Allow inbound  http traffic"
+  }
+}
