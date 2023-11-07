de0e74f0b5b9f8dfd3b23e6f9b691f2489e0b8dc
Valappil <mvalappil@clinipace.com>
2023-11-07 08:01:22 +0530

    sg

--- security.tf
+++ security.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "management" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["172.16.0.0/24"]
+    cidr_blocks = ["0.0.0.0/24"]
   }
 
 
