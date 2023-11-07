15c1a679bcd79c07de201b2ae2c32e5e32590de5
Valappil <mvalappil@clinipace.com>
2023-11-07 08:12:33 +0530

    tt

--- security.tf
+++ security.tf
@@ -11,6 +11,13 @@ resource "aws_security_group" "management" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 
   tags = {
     Name = "allow inbound ssh"
